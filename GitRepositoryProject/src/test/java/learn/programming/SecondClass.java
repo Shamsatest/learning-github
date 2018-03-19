75050cfdf6643ee7fab273b1443f3efb235a8bba
Shamsa <Shamsa@DESKTOP-31T2EOK.home>
2018-03-18 23:34:56 -0400

    Adding new java class.

--- GitRepositoryProject/src/test/java/learn/programming/SecondClass.java
+++ GitRepositoryProject/src/test/java/learn/programming/SecondClass.java
@@ -1,9 +1,10 @@
 package learn.programming;
 
-public class Github {
+public class SecondClass {
 
 	public static void main(String[] args) {
-System.out.println("Learning new things is fun");
+		// TODO Auto-generated method stub
+
 	}
 
 }
